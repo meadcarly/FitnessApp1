ee68ae29546bbc594a0cceb1f9e0347fff9399f1
Carly Mead <meadcarly16@gmail.com>
2024-10-25 14:58:42 -0600

    Added the link to access Goals

--- FitnessApp1/obj/Debug/net8.0/FitnessApp1.AssemblyInfo.cs
+++ FitnessApp1/obj/Debug/net8.0/FitnessApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d927043023eda2a17f384a6637aac15e046d4d51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2410d79c87a797d24e3133b239c4e96680b20b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
